86607aff2bceda160de327445ff02868fb65f6f1
dianadudic <dianadudic@gmail.com>
2020-11-12 10:45:41 -0500

    added some changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Good morning, last day of Git");
+        //working on branch
+        //and observing the head
     }
 }
